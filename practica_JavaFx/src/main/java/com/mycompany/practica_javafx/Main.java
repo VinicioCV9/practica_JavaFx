86c25be0c25cafcd8a80450a8e2675cd5156c4fa
umg <umg@HUE-LAB-2B3.labhue.local>
2026-05-09 10:47:21 -0600

    Integrate service with UI

--- practica_JavaFx/src/main/java/com/mycompany/practica_javafx/Main.java
+++ practica_JavaFx/src/main/java/com/mycompany/practica_javafx/Main.java
@@ -3,6 +3,7 @@
  */
 package com.mycompany.practica_javafx;
 import com.practica.productos.modelo.Producto;
+import com.practica.productos.servicio.ProductoService;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,16 +13,23 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    ProductoService servicio = new ProductoService();
+
     @Override
     public void start(Stage stage) {
         TextField campo = new TextField();
-        Button boton = new Button("Mostrar");
+        Button boton = new Button("Agregar");
         Label label = new Label();
 
         boton.setOnAction(e -> {
             try {
-                Producto p = new Producto(campo.getText());
-                label.setText(p.getNombre());
+                servicio.agregar(new Producto(campo.getText()));
+                String texto = "";
+                for (Producto p : servicio.listar()) {
+                    texto += p.getNombre() + "\n";
+                }
+                label.setText(texto);
             } catch (Exception ex) {
                 label.setText(ex.getMessage());
             }
